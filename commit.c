281f58a6c6a8526a563b3477e275ff39653fa971
Abhijithmns <abhijithmns07@gmail.com>
2026-04-21 17:21:35 +0530

    Phase 4: add commit serialization and object_write in commit_create

--- commit.c
+++ commit.c
@@ -14,7 +14,6 @@
 // PROVIDED functions: commit_parse, commit_serialize, commit_walk, head_read, head_update
 // TODO functions:     commit_create
 
-
 #include "commit.h"
 #include "tree.h"
 #include <stdio.h>
