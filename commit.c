19b79467b5c0265b2e9e9c3a846b121f22d924c8
Abhijithmns <abhijithmns07@gmail.com>
2026-04-21 17:22:05 +0530

    Phase 4: add head_update to advance branch pointer after commit

--- commit.c
+++ commit.c
@@ -13,6 +13,7 @@
 //
 // PROVIDED functions: commit_parse, commit_serialize, commit_walk, head_read, head_update
 // TODO functions:     commit_create
+//FINAL 
 
 #include "commit.h"
 #include "tree.h"
